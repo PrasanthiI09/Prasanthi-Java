4a3c9805f0b7172954f0e966cc552adbba39130e
PrasanthiI09 <prasanthi.indrakanti@gmail.com>
2024-11-06 02:41:44 +0530

    Add files via upload

---
+++ Ifstatementtask1.java
@@ -0,0 +1,25 @@
+package javatask4;
+
+public class Ifstatementtask1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = 35;
+		int b=89;
+		int c=56;
+		int d=37;
+		int sum1 = a+b;
+		int sum2 = c+d;
+		if(sum1>sum2) {
+			System.out.println(sum1 +" is greater than "+sum2);
+			
+		}
+		else {
+		System.out.println(sum2 +" is greater than "+ sum1);
+		}
+		
+	}
+
+}
